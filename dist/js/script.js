8fcbe49db8b2f8dcba3c7e4f3e3458ae3a66090e
wahyu firmansyah <whyfirmansyah69@gmail.com>
2023-10-16 13:39:33 +0700

    menambahkan file untuk pertama kali

---
+++ dist/js/script.js
@@ -0,0 +1,22 @@
+// navbar fixed
+window.onscroll = function() {
+    const header = document.querySelector('header');
+    const fixedNav = header.offsetTop;
+
+    if (window.pageYOffset > fixedNav){
+        header.classList.add('navbar-fixed');
+    } else {
+        header.classList.remove('navbar-fixed');
+    }
+};
+
+// bagian hamburger
+const hamburger = document.querySelector('#hamburger');
+// ambil nav menu
+const navMenu = document.querySelector('#nav-menu');
+hamburger.addEventListener('click', function(){
+    hamburger.classList.toggle('hamburger-active');
+    // nav menu
+    navMenu.classList.toggle('hidden');
+
+});
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container: {
+      center: true,
+      padding: '32px',
+    },
+    extend: {
+      colors: {
+        primary: '#1c283c',
+        secondary: '#64748b',  
+        dark: '#172030',
+        dongker: '#03537d'
+      },
+      screens: {
+        '2xl': '1720px',
+      }
+    },
+  },
+  plugins: [],
+}
